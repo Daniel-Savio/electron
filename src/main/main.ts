26baa35f7e149a90616637ed64c7a62019d56eec
Daniel Sávio <spnjc137@gmail.com>
2024-01-17 22:38:35 -0300

    build: recreating everything to put in a fast start with electron, IPC-Protocol, React and Tailwind

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,25 +1,48 @@
-/* eslint global-require: off, no-console: off, promise/always-return: off */
-
-/**
- * This module executes inside of electron's main process. You can start
- * electron renderer process from here and communicate with the other processes
- * through IPC.
- *
- * When running `npm run build` or `npm run build:main`, this file is compiled to
- * `./src/main.js` using webpack. This gives us some performance wins.
- */
 import path from 'path';
-import { app, BrowserWindow, shell } from 'electron';
+import { app, BrowserWindow, shell, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
+import log from 'electron-log';
+import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
-
+import SCL from './scl'
+import XML from './xml';
+import fs from 'fs'
+class AppUpdater {
+  constructor() {
+    log.transports.file.level = 'info';
+    autoUpdater.logger = log;
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+}
 let mainWindow: BrowserWindow | null = null;
-
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
   sourceMapSupport.install();
 }
 
+
+const isDebug = true;
+
+if (isDebug) {
+  require('electron-debug')();
+}
+const installExtensions = async () => {
+  const installer = require('electron-devtools-installer');
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+  const extensions = ['REACT_DEVELOPER_TOOLS'];
+
+  return installer
+    .default(
+      extensions.map((name) => installer[name]),
+      forceDownload
+    )
+    .catch(console.log);
+};
 const createWindow = async () => {
+  if (isDebug) {
+    await installExtensions();
+  }
+
   const RESOURCES_PATH = app.isPackaged
     ? path.join(process.resourcesPath, 'assets')
     : path.join(__dirname, '../../assets');
@@ -30,9 +53,17 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1624,
+    height: 828,
     icon: getAssetPath('icon.png'),
+    frame: false,
+    webPreferences: {
+      preload: app.isPackaged
+        ? path.join(__dirname, './preload.js')
+        : path.join(__dirname, '../../.erb/dll/preload.js'),
+        devTools: true
+       
+    },
   });
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
@@ -52,12 +83,56 @@ const createWindow = async () => {
     mainWindow = null;
   });
 
-  // Open urls in the user's browser
+  // Create a menu bar
+  // const menuBuilder = new MenuBuilder(mainWindow);
+  // menuBuilder.buildMenu();
+
   mainWindow.webContents.setWindowOpenHandler((edata) => {
     shell.openExternal(edata.url);
     return { action: 'deny' };
   });
+
+  // Remove this if your app does not use auto updates
+  // eslint-disable-next-line
+  new AppUpdater();
 };
+
+
+
+
+
+// Window controller
+ipcMain.on('closeApp', async (event, arg) => {
+    mainWindow!.close()
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+});
+ipcMain.on('minimize', async (event, arg) => {
+    mainWindow!.minimize()
+
+});
+ipcMain.on('maximize', async (event, arg) => {
+    if(mainWindow!.isMaximized()){
+      mainWindow!.restore();
+    }else{
+      mainWindow!.maximize();
+      console.log("minimized");
+    }
+    
+});
+
+ipcMain.on('askFor', async (event, arg) => {
+  if(arg === "scl") 
+  mainWindow!.webContents.send("scl", "Resposta do Backend");
+  
+});
+
+ipcMain.on('askFor', async (event, arg) => {
+  if(arg === 'files'){
+    mainWindow!.webContents.send('files', "Outra resposta do Backend")
+  }
+})
 
 app.on('window-all-closed', () => {
   // Respect the OSX convention of having the application in memory even
@@ -67,14 +142,15 @@ app.on('window-all-closed', () => {
   }
 });
 
-app
-  .whenReady()
-  .then(() => {
+app.whenReady().then(() => {
     createWindow();
+
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
       if (mainWindow === null) createWindow();
     });
-  })
-  .catch(console.log);
+  }).catch(console.log);
+
+
+
---
+++ src/main/preload.ts
@@ -0,0 +1,34 @@
+import { ipcRenderer } from "electron"
+const { contextBridge } = require('electron');
+
+
+console.log("preloaded file")
+
+const electronHandler = {
+  //First value is the channel and the scond is the data
+  close: () => ipcRenderer.send("closeApp", "closeApp"),
+  minimize: () => ipcRenderer.send('minimize', 'minimize'),
+  maximize: () => ipcRenderer.send('maximize', 'maximize'),
+  devTools: () => ipcRenderer.send('devTools', 'devTools'),
+
+
+  askForScl: () => ipcRenderer.send('askFor', 'scl'),
+  scl: (callback:any) => ipcRenderer.on('scl', (events, args)=>{
+    callback(args);
+  }),
+
+  askForFiles: () => ipcRenderer.send('askFor', 'files'),
+  files:(callback: any) => ipcRenderer.on('files', (events, args: Array<any>)=>{
+    callback(args);
+  }),
+
+
+  on: (channel: string, func: any) => ipcRenderer.send(channel, (evt: any, ...args: any[]) =>func(...args)),
+  
+}
+
+
+
+contextBridge.exposeInMainWorld('electron', electronHandler);
+
+export type ElectronHandler = typeof electronHandler;
